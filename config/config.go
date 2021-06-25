78242d8ef815823b7964c6af1cef5b51b5d8c232
Eric Fried <efried@redhat.com>
2021-06-25 08:01:49 -0500

    Add config.go
    I guess this was part of the osdk skeleton at an earlier version.
    Boilerplate's project.mk is still set up to use this, so add it for now.

---
+++ config/config.go
@@ -0,0 +1,10 @@
+package config
+
+const (
+	// OperatorName stores the name used by this code for the Hive Health Operator
+	OperatorName string = "hive-health-operator"
+
+	// OperatorNamespace stores a string indicating the Kubernetes namespace in which the operator runs
+	OperatorNamespace string = "hive-health-operator"
+)
+
